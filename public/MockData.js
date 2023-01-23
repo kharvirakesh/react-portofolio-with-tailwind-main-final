7251e8319dcc6504379ae3974f3d8ea9ea988672
kharvirakesh3 <kharvirakesh3@gmail.com>
2023-01-23 17:14:20 +0530

    FEAT : first commit

---
+++ public/MockData.js
@@ -0,0 +1,17 @@
+export const Data = [{
+    "ProjectName": "Dentamitra WebApp",
+    "ProjectDescription1": "Built an web application led by by Bizydales.net using ReactJs which helps end user to Book an appointment through online. Denta Mitra brings a Dentist to your home and offers 24x7 video consultation.",
+    "ProjectDescription2": "DentaMitra provides tips on dental health, reminders, SMS on daily dental hygiene routine, check teeth movements, calculate sweet score, see nearby dentists through Google map And also I have integrated Razorpay payment gateway for the mobile application in the release",
+    "Technology Used": ["React-Js", "Ant Design", "Node JS"]
+},
+{
+    "ProjectName": "BlueOcean WebApp",
+    "ProjectDescription1": "Built an web application led by by Bizydales.net using ReactJs which helps end user to Book an appointment through online. Denta Mitra brings a Dentist to your home and offers 24x7 video consultation.",
+    "ProjectDescription2": "DentaMitra provides tips on dental health, reminders, SMS on daily dental hygiene routine, check teeth movements, calculate sweet score, see nearby dentists through Google map And also I have integrated Razorpay payment gateway for the mobile application in the release",
+    "Technology Used": ["React-Js", "Ant Design", "Node JS"]
+}, {
+    "ProjectName": "Dummy WebApp",
+    "ProjectDescription1": "Built an web application led by by Bizydales.net using ReactJs which helps end user to Book an appointment through online. Denta Mitra brings a Dentist to your home and offers 24x7 video consultation.",
+    "ProjectDescription2": "DentaMitra provides tips on dental health, reminders, SMS on daily dental hygiene routine, check teeth movements, calculate sweet score, see nearby dentists through Google map And also I have integrated Razorpay payment gateway for the mobile application in the release",
+    "Technology Used": ["React-Js", "Ant Design", "Node JS"]
+}]
