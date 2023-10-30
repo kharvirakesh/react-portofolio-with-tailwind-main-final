6ffec80d70b259314f2a6cd944a4af928f78db39
kharvirakesh <kharvirakesh3.com>
2023-10-30 23:38:28 +0530

    FEAT : constants added and moved all the hardcoded intro data

---
+++ constants/index.js
@@ -0,0 +1,13 @@
+const aboutMe = [
+  {
+    name: "Rakesh Kharvi",
+    role: "Software Developer Engineer.",
+    description:
+      "I help people to tell their stories on the web. Join me down below and let's get cracking!",
+    "githubURL":"https://github.com/kharvirakesh",
+    "linkedInURL":"https://www.linkedin.com/in/rakesh-kharvi/",
+    "avatar":"",
+    "services":"Since the beginning of my journey as a web developer, I've worked for MNC corporate companies, startups and collaborated with talanted people to create digital products for both business and consumer use.",
+
+  },
+];
