59b557c75a8d305a7b6433a2f8ace34da7316543
kharvirakesh <kharvirakesh3.com>
2023-10-30 23:58:37 +0530

    FIX : code refactored

---
+++ components/Introduction/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Link from "next/link";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import Image from "next/image";
+import { aboutMe } from "../../constants";
+
+const { name, role, description, githubURL, linkedInURL, avatar } = aboutMe[0];
+
+const IntroCompo = () => {
+  return (
+    <div>
+      <div className="text-center p-10 py-10">
+        <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+          {name ?? ""}
+        </h2>
+        <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+          {role ?? ""}
+        </h3>
+        <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+          {description ?? ""}
+        </p>
+        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+          <Link href={`${githubURL}`}>
+            <a target="_blank" rel="noopener noreferrer">
+              <AiFillGithub />
+            </a>
+          </Link>
+
+          <Link href={`${linkedInURL}`}>
+            <a target="_blank" rel="noopener noreferrer">
+              <AiFillLinkedin />
+            </a>
+          </Link>
+        </div>
+        <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+          <Image src={avatar} layout="fill" objectFit="cover" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IntroCompo;
--- constants/index.js
+++ constants/index.js
@@ -1,13 +1,13 @@
-const aboutMe = [
+export const aboutMe = [
   {
     name: "Rakesh Kharvi",
     role: "Software Developer Engineer.",
     description:
       "I help people to tell their stories on the web. Join me down below and let's get cracking!",
-    "githubURL":"https://github.com/kharvirakesh",
-    "linkedInURL":"https://www.linkedin.com/in/rakesh-kharvi/",
-    "avatar":"",
-    "services":"Since the beginning of my journey as a web developer, I've worked for MNC corporate companies, startups and collaborated with talanted people to create digital products for both business and consumer use.",
-
+    githubURL: "https://github.com/kharvirakesh",
+    linkedInURL: "https://www.linkedin.com/in/rakesh-kharvi/",
+    avatar: require("../public/rakesh.png"),
+    services:
+      "Since the beginning of my journey as a web developer, I've worked for MNC corporate companies, startups and collaborated with talanted people to create digital products for both business and consumer use.",
   },
 ];
